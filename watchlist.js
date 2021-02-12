67c266d79c2349ed2908484590b72248c7a277a2
Jake Luecke <jrluecke95@gmail.com>
2021-02-12 17:14:41 -0500

    started styling page

--- watchlist.js
+++ watchlist.js
@@ -8,9 +8,9 @@ $(document).ready(function () {
     const $movieContainer = $('.movies-container')
     function renderMovies(movieArray) {
         const $movieHTMLArray = movieArray.map(currentMovie => {
-            return `<div class="col-4 mb-3 mt-3">
-            <div class="card" style="width: 18rem;">
-                <img src="${currentMovie.Poster}" class="card-img-top" alt="${currentMovie.Title}">
+            return `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 mt-3">
+            <div class="card w-100">
+                <img src="${currentMovie.Poster}" class="card-img-top w-100" alt="${currentMovie.Title}">
                 <div class="card-body">
                     <h5 class="card-title">${currentMovie.Title}</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of
