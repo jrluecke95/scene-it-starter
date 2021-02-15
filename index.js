ee06525dfabfb6030842ba5ca2f8cd2ba4a06b1a
Jake Luecke <jrluecke95@gmail.com>
2021-02-15 17:24:20 -0500

    got add movie button working kind of

--- index.js
+++ index.js
@@ -25,32 +25,33 @@ $(document).ready(function () {
     const $movieContainer = $('.movies-container')
     function renderMovies($movieArray) {
         const $movieHTMLArray = $movieArray.map(currentMovie => {
+            let titles = []
             for (let i = 0; i < watchlist.length; i++) {
-                console.log(i)
-                if (currentMovie.Title === watchlist[i].Title) {
-                    return `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 mt-3">
-                    <div class="card w-100">
-                        <img src="${currentMovie.Poster}" class="card-img-top w-100" alt="${currentMovie.Title}">
-                        <div class="card-body">
-                            <h5 class="card-title">${currentMovie.Title}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                                the card's content.</p>
-                        </div>
+                titles.push(watchlist[i].Title)
+            }
+            if ($.inArray(currentMovie.Title, titles) !== -1) {
+                return `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 mt-3">
+                <div class="card w-100">
+                    <img src="${currentMovie.Poster}" class="card-img-top w-100" alt="${currentMovie.Title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${currentMovie.Title}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
                     </div>
-                </div>`
-                } else {
-                    return `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 mt-3">
-                    <div class="card w-100">
-                        <img src="${currentMovie.Poster}" class="card-img-top w-100" alt="${currentMovie.Title}">
-                        <div class="card-body">
-                            <h5 class="card-title">${currentMovie.Title}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                                the card's content.</p>
-                            <a href="#" onclick="saveToWatchList('${currentMovie.imdbID}')" class="btn btn-primary">Add Movie</a>
-                        </div>
+                </div>
+            </div>`
+            } else {
+                return `<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 mt-3">
+                <div class="card w-100">
+                    <img src="${currentMovie.Poster}" class="card-img-top w-100" alt="${currentMovie.Title}">
+                    <div class="card-body">
+                        <h5 class="card-title">${currentMovie.Title}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" onclick="saveToWatchList('${currentMovie.imdbID}')" class="btn btn-primary">Add Movie</a>
                     </div>
-                </div>`
-                }
+                </div>
+            </div>`
             }
         })
         return $movieHTMLArray.join('')
